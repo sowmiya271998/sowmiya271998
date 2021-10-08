f1d341de9a81fb9b13e5ab5695cc52f109d303a9
sowmiya271998 <43902562+sowmiya271998@users.noreply.github.com>
2021-10-08 09:40:05 +0530

    Add files via upload

---
+++ StringBuffer.java
@@ -0,0 +1,11 @@
+class StringBuffer
+{
+	public static void main(String[] args) 
+	{
+		StringBuilder a=new StringBuilder("Sowmiya");
+		System.out.println("Oroginal String is : "+a);
+		a.append(" "+"Sekar"+".");
+		System.out.println("Modified String is : "+a+" K");
+		
+	}
+}
